2c0021de38e9b87b0f1379d87592cd412a724682
Marek Truska <truskamarek@centrum.sk>
2022-09-16 15:10:53 +0000

    Added plan vars and params to home page

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,5 +1,8 @@
 class PagesController < ApplicationController
+    # Get request for '/' which is our home page
     def home
+        @basic_plan = Plan.find(1)
+        @premium_plan = Plan.find(2)
     end
     
     def about
