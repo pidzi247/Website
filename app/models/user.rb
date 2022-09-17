bd1ed9d6cdb51f959bef7324ea1ad85731c32dbc
Marek Truska <truskamarek@centrum.sk>
2022-09-17 18:56:52 +0000

    Updated flash message styles

--- app/models/user.rb
+++ app/models/user.rb
@@ -14,7 +14,7 @@ class User < ApplicationRecord
   # Store customer.id as the customer token and save the user.
   def save_with_subscription
     if valid?
-      customer = Stripe::Customer.create(description: email, plan: plan_id, card: stripe_card_token)
+      customer = Stripe::Customer.create(email: email, source: stripe_card_token)
       self.stripe_customer_token = customer.id
       save!
     end
