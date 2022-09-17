3c044273d3bc1efb994ec3096476ab8acc001623
Marek Truska <truskamarek@centrum.sk>
2022-09-17 17:37:17 +0000

    Added comments for Devise and Stripe actions

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -1,4 +1,8 @@
 class Users::RegistrationsController < Devise::RegistrationsController
+  # Extend default Devise gem behavior so that
+  # users signing up with the Premium account 
+  # save with a special Stripe subscription function.
+  # Otherwise Devise signs up user as usual.
   def create
     super do |resource|
       if params[:plan]
